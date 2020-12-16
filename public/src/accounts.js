3b56ae8a508b6056a058417ac8594579d6c1e59a
unknown <remusblee@gmail.com>
2020-12-16 16:49:42 -0500

    first commit to project-local-library

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,75 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find(account=> account.id === id)
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  let mapped = accounts.map((name) => name.name.last)
+   // console.log(mapped)
+  let sorted = mapped.sort((key1,key2) => {
+    return key1.localeCompare(key2)
+  })
+//  console.log(sorted)
+  
+  let test = sorted.map((lastName) => {
+    return {name: { first: lastName, last: lastName}}    
+ 
+  })
+  //console.log(test)
+  let testmapped = accounts.map((name)=> name.name)
+  //console.log(testmapped)
+  let testsort = testmapped.sort((key1,key2)=>{
+    return key1.last.localeCompare(key2.last)    
+  })
+  
+  //console.log(testsort)
+  let supermap = testsort.map((name)=> {  //why does putting name put name:
+    return {name}
+  })
+  return(supermap)
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  return books.reduce((acc,book)=>{
+    const count = book.borrows.reduce((borrowAcc, borrow)=>{ //access the array in the borrows
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;       
+    },0);
+    return acc + count
+  },0)
+}
 
-function booksInPossession(account, books, authors) {}
+function booksInPossession(account, books, authors) {
+  /*let possessedBooks = []
+  for(let i = 0; i < books.length; i++){   //cycle through all books
+    const {id, title, genre, borrows} = books[i] //destructuring
+    for(let j = 0; j < borrows.length; j++) {   //go through borrowing info to see whats borrowed 
+      if(borrows[j].id === account.id && borrows[j].returned ===false){ //if book is not returned, go through another loop
+        for(let k = 0 ; k < authors.length; k++){
+          let author = authors[k]
+          //compare author id associate with the book
+          if(author.id === books[i].authorId){ //compare author id to book id, create another object to push 
+            let tempBook = {id, title, genre, author, borrows} //shorthand creating an object
+            possessedBooks.push(tempBook)
+          }
+          
+        }
+      }
+      
+      
+    }   
+    
+    
+  }  
+  
+  return possessedBooks */
+  return books.filter((book)=> {
+    const recent = book.borrows[0];
+    return !recent.returned && recent.id === account.id   
+  })
+  .map(book=> {
+    const author = authors.find(author=>author.id === book.authorId);
+    return {...book, author};
+  })  
+}
 
 module.exports = {
   findAccountById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,151 @@
-function totalBooksCount(books) {}
+function totalBooksCount(books) {
+  //   let counter = 0
+//   for (book in books){
+//     counter = counter+1
+//   }
+//   return counter
+return books.length
+}
 
-function totalAccountsCount(accounts) {}
+function totalAccountsCount(accounts) {
+  //   let counter = 0
+//   for (account in accounts){
+//     counter = counter + 1
+//   }
+//   return counter
+return accounts.length
+}
 
-function booksBorrowedCount(books) {}
+function booksBorrowedCount(books) {
+  let counter = 0 
+  console.log(books)
+  for (let book of books){  //changing book in to book of fixed it because books was an array for some reason   
+    if (book.borrows[0].returned === false){
+      counter = counter + 1
+    }
+  }
+  return counter
+}
 
-function mostCommonGenres(books) {}
+function mostCommonGenres(books) {
+  //sort from most common to least common genre
+  //books is in an array, loop through books to get genre
+  let countedBooks = books.reduce((acc, item) => {
+    //     if (!acc[item.genre]){     //creates empty array, maybe not needed
+    //       console.log('asdf')
+    //       acc[item.genre] = []
+    //     }
+        if (acc[item.genre]){
+          console.log('123')
+          acc[item.genre]++     //how do u do this with = + 1    
+          }    
+        else {
+          acc[item.genre] = 1
+        }
+        return acc
+      },{})
+      console.log(countedBooks)  //gives an object {'Historical Fiction': 1,   Science: 3, etc}
+        let keys = Object.keys(countedBooks) //pull out the keys [historical fiction, science] use this to loop! 
+        let sortedKeys = keys.sort((key1,key2) => {
+          return countedBooks[key2] - countedBooks[key1]     //standard way to sort, trick is using countedBooks
+                //sorted keys is order largest to smallest [science, classics, etc]
+        })
+        
+        let mapped = sortedKeys.map((key) => {
+          return {name:key, count: countedBooks[key]} //turns array -> array of objects
+          
+        })
+      console.log(sortedKeys)
+      console.log(mapped)
+      
+        
+    //   }, {})
+      return mapped.slice(0,5)  //slice array from index 0 to index 5 not including index 5
+}
 
-function mostPopularBooks(books) {}
+function mostPopularBooks(books) {
+   //return array with 5 or less objects, most popular to least. 
+  //popular = # of times book has been borrowed
+  
+  //use reduce to count # of borrows
+  //use borrows.length
+  let countedBorrows = books.reduce((acc,item) => {     
+    acc[item.title] = item.borrows.length          //adding the title and the count to the object    
+  return acc
+}, {})
+console.log(countedBorrows, 'yo')
 
-function mostPopularAuthors(books, authors) {}
+let keys = Object.keys(countedBorrows) //array of keys ['asdf','asdf']
+console.log(keys)
+let sortedKeys = keys.sort((key1,key2) => {
+  return countedBorrows[key2] - countedBorrows[key1]
+})
+//now that keys are sorted, use map to create the object
+let mapped = sortedKeys.map((key) => {
+ return  {name: key, count: countedBorrows[key]}
+      
+})
+
+console.log(mapped)
+return mapped.slice(0,5)
+}
+
+function mostPopularAuthors(books, authors) {
+   //find the most popular authors, popular = checked out the most
+  // need to add up all the books with the same author
+  
+  //loop through books and total up amount of borrows
+  let countedBorrows = books.reduce((acc,item) => {     
+    acc[item.authorId] = item.borrows.length         //adding the title and the count to the object    
+  return acc
+}, {}) 
+
+
+
+console.log(countedBorrows, 'line 113')
+//to reduce the array
+//   for (let id in countedBorrows){
+//     let sum = countedBorrows[id].reduce((acc,item)=> {
+//       return acc + item      
+//     })
+//     countedBorrows[id] = sum
+//   }
+
+
+console.log(countedBorrows)
+let keys = Object.keys(countedBorrows)
+let sortedKeys = keys.sort((key1,key2) => {
+  return countedBorrows[key2] - countedBorrows[key1]  //array of author ids sorted
+  
+})
+console.log(sortedKeys)
+
+let mappedKeys = sortedKeys.map(key =>{
+  const aut = authors.find((val) =>{
+    return val.id === Number(key)      
+  })
+  return {name: `${aut.name.first} ${aut.name.last}`, count: countedBorrows[key]}    
+  
+})
+
+console.log(mappedKeys)
+return mappedKeys.slice(0,5)
+//   for (let i =  0 ; i < authors.length; i++){
+//     let keyNames = []
+//     if (sortedKeys[i] === authors.id){
+//       keyNames.push('asdf')
+//       console.log(keyNames)
+//     }
+//   let filteredID  = authors.filter((name) =>{
+//     for (let i = 0 ; i < sortedKeys.length){
+//       if (sortedKeys[i] === authors)
+//     }
+                                 
+  
+  
+//   })    
+
+}
 
 module.exports = {
   totalBooksCount,
